323e242e9921316a78e2af0d2816eca1e0bbb2ee
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-06 14:34:12 +0200

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: "/freeunlimitedparking",
+  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
 });
